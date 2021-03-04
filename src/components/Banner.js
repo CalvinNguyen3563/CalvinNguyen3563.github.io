d5a9359f2acb1f10091bc0a81758a53028cdee0f
Anh Ngo <code@anhngo.me>
2021-03-03 23:37:41 -0800

    change title

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,11 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Athos Invest</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>We Strive for Rapid Growth<br />
+                To Achieve Financial Security</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
